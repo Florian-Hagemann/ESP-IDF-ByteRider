5e47140106472001422d3aa2be6e696d8bd44af8
Florian-Hagemann <florianhagemann42@gmail.com>
2026-06-03 14:36:00 +0200

    br_led_driver: Starting on function implementations

--- components/br_led_driver/br_led_driver.c
+++ components/br_led_driver/br_led_driver.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include "br_led_driver.h"
+#include <stdint.h>
+#include <stdbool.h>
 
-void func(void)
-{
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
 
+static const char* TAG = "br_led_driver"; // Tag for logs
+static TaskHandle_t br_led_driver_task_handle = NULL;
+
+// Driver task
+static void br_led_driver_task(void *pvArguments) {
+    ESP_LOGI("Created br_led_driver task.")
 }
+
+// Create driver task
+void br_led_driver_init(void);
+
+// Start driver task (if stopped)
+void br_led_driver_start(void); 
+
+// Stop driver task
+void br_led_driver_stop(void);
+
+
+void br_led_driver_kill(void);  
+
+void br_led_driver_set_pattern(uint16_t pattern);   
+void br_led_driver_set_d11(bool state);             
