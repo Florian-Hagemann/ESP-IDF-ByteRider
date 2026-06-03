c4072572b51937313260fd1d9a9ce092cba7b6b9
Florian-Hagemann <florianhagemann42@gmail.com>
2026-06-03 14:29:00 +0200

    br_led_driver: Add function definitions in header

--- components/br_led_driver/include/br_led_driver.h
+++ components/br_led_driver/include/br_led_driver.h
@@ -1,1 +1,14 @@
-void func(void);
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// Functions for the driver task
+void br_led_driver_init(void);  // initialize LED
+void br_led_driver_start(void); // start driver task
+void br_led_driver_stop(void);  // stop driver task
+void br_led_driver_kill(void);  // kill driver task
+
+// Functions for controlling LEDs
+void br_led_driver_set_pattern(uint16_t pattern);   // writes the pattern to the shift register
+void br_led_driver_set_d11(bool state);             // turns d11 and d11_2 on or off
