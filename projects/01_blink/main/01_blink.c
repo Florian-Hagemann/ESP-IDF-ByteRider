ba6bbc5f7f2a55d59f9f0a1ac1a88cec07506631
Florian-Hagemann <florianhagemann42@gmail.com>
2026-06-04 22:44:09 +0200

    01_blink: Finished project and fixed CMake issues in drivers

--- projects/01_blink/main/01_blink.c
+++ projects/01_blink/main/01_blink.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "br_led_driver.h"
 
 void app_main(void)
 {
+    br_led_driver_init();
 
+    while(1) {
+
+        br_led_driver_set_pattern((uint16_t)0xFFFF);
+        br_led_driver_set_d11(0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        br_led_driver_set_pattern((uint16_t)0x0000);
+        br_led_driver_set_d11(1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    }
+    
 }
