314375caf3874282b47fec5995a9515597a24621
Florian-Hagemann <florianhagemann42@gmail.com>
2026-06-04 17:47:14 +0200

    br_led_driver: Implement init function

--- components/br_led_driver/br_led_driver.c
+++ components/br_led_driver/br_led_driver.c
@@ -8,15 +8,29 @@
 #include "esp_log.h"
 
 static const char* TAG = "br_led_driver"; // Tag for logs
-static TaskHandle_t br_led_driver_task_handle = NULL;
+static TaskHandle_t br_led_driver_task_handle = NULL; // Task handle for driver task
 
 // Driver task
 static void br_led_driver_task(void *pvArguments) {
-    ESP_LOGI("Created br_led_driver task.")
+    ESP_LOGD(TAG, "Successfully created br_led_driver task.");
+    
+    while(1) {
+
+    }
 }
 
 // Create driver task
-void br_led_driver_init(void);
+void br_led_driver_init(void) {
+    ESP_LOGD(TAG, "Creating br_led_driver task...");
+    xTaskCreate(
+        br_led_driver_task,
+        "br_led_driver_task",
+        3072,
+        NULL,
+        1,
+        &br_led_driver_task_handle
+    );
+}
 
 // Start driver task (if stopped)
 void br_led_driver_start(void); 
@@ -24,7 +38,7 @@ void br_led_driver_start(void);
 // Stop driver task
 void br_led_driver_stop(void);
 
-
+// Kill driver task
 void br_led_driver_kill(void);  
 
 void br_led_driver_set_pattern(uint16_t pattern);   
