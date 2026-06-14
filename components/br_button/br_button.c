8a2f9b8daef08335448795e9ed62f574f051476c
Florian-Hagemann <florianhagemann42@gmail.com>
2026-06-14 12:52:36 +0200

    br_button: Add includes for headers and add TODOs

--- components/br_button/br_button.c
+++ components/br_button/br_button.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include "br_button.h"
+#include "br_board_config.h"
 
-void func(void)
-{
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 
+#include "esp_log.h"
+#include "driver/gpio.h"
+
+static const char* TAG = "br_button";
+
+void br_button_setup(button_config_t *button_config) {
+    ESP_LOGD(TAG, "Setting up button...");
+    // TODO: Setup ISR
+    // TODO: Setup Button handler to debounce and run the onpress func
 }
