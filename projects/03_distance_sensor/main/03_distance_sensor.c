5fd72488986e1cf1d68103d7b9b88c707f32a31e
Florian-Hagemann <florianhagemann42@gmail.com>
2026-07-02 16:39:02 +0200

    03_distance_sensor: finish project to make leds show distance

--- projects/03_distance_sensor/main/03_distance_sensor.c
+++ projects/03_distance_sensor/main/03_distance_sensor.c
@@ -8,9 +8,27 @@
 void app_main(void)
 {   
     br_sonar_sensor_init();
+    br_led_driver_init();
+    
+    float distance;
+
     while(1) {
-        ESP_LOGI("TAG", "%f cm", br_sonar_sensor_getDistance());
-        vTaskDelay(200);
+        
+        distance = br_sonar_sensor_getDistance();
+
+        if(distance < 10) {
+            br_led_driver_set_pattern(0x0000);
+        } else if (distance < 20) {
+            br_led_driver_set_pattern(0x000F);
+        } else if (distance < 30) {
+            br_led_driver_set_pattern(0x00FF);
+        } else if (distance < 40) {
+            br_led_driver_set_pattern(0x0FFF);
+        } else {
+            br_led_driver_set_pattern(0xFFFF);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 
 }
